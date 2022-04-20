d19b3f7718e96aa271abee3e5508a31b74e0ab35
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-04-20 16:29:17 +0200

    Adding tostring() method

--- src/main/java/se/Lexicon/App.java
+++ src/main/java/se/Lexicon/App.java
@@ -15,13 +15,12 @@ public class App
         TodoItem task1=new TodoItem(1,"task1","Inheritance",mehrdad);
 
         TodoItemTask taskForSudipta = new TodoItemTask(2,task1,sudipta);
-
+        System.out.println(taskForSudipta);
         Person sandip= new Person(3,"Sandip","Mohapatra","sandip@gmail.com");
         TodoItem task2=new TodoItem(2,"task2","workload",sudipta);
         TodoItemTask taskForSandip=new TodoItemTask(3,task2,sandip);
 
-        System.out.println(sudipta.getSummery()+task1);
-        System.out.println(task1);
+        System.out.println(taskForSandip);
     }
 
 
