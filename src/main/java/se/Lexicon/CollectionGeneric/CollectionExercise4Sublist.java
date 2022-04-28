4649c6af807d98e163b9af4a48984f382533c7df
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-04-28 15:52:11 +0200

    Collection Exercises

---
+++ src/main/java/se/Lexicon/CollectionGeneric/CollectionExercise4Sublist.java
@@ -0,0 +1,10 @@
+package se.Lexicon.CollectionGeneric;
+
+import java.util.List;
+
+public class CollectionExercise4Sublist extends CollectionExercise4{
+    public static void main(String[] args) {
+
+
+    }
+}
